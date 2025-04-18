2ccdfe138a5701bd9295661c774b6c219aaa2c43
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-04-18 17:33:59 +0900

    Feat: Add HGText component 모든 텍스트는 HGText 컴포넌트를 사용합니다. merge 기능 추가 필요

---
+++ components/text/HGText.tsx
@@ -0,0 +1,187 @@
+import { View, Text } from "react-native";
+import React from "react";
+import clsx from "clsx";
+
+type HGTextType =
+    | "btn"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "p1"
+    | "p1_L"
+    | "p2"
+    | "p2_L"
+    | "p3"
+    | "p3_L"
+    | "p4"
+    | "p5"
+    | "point"
+    | "title";
+
+interface Props {
+    variant: HGTextType;
+    text: string;
+    className?: string;
+}
+
+const HGText = ({ variant, text, className }: Props) => {
+    switch (variant) {
+        case "btn":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-semibold text-[24px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "h1":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-semibold text-[24px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "h2":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-semibold text-[20px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "h3":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-semibold text-[16px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "h4":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-semibold text-[14px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p1":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-medium text-[16px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p1_L":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-regular text-[16px] leading-[1.5]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p2":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-medium text-[14px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p2_L":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-light text-[14px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p3":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-medium text-[12px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+
+        case "p3_L":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-regular text-[12px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p4":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-regular text-[10px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "p5":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-regular text-[8px] leading-[1.4]"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "point":
+            return (
+                <Text
+                    className={clsx(
+                        "font-pretendard-semibold text-[40px] leading-none"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+        case "title":
+            return (
+                <Text
+                    className={clsx(
+                        "font-gmarket-medium text-[40px] leading-none"
+                    )}
+                >
+                    {text}
+                </Text>
+            );
+
+        default:
+            return <Text className={clsx()}>{text}</Text>;
+    }
+};
+
+export default HGText;
