74dce81c15bfcf0bc78f62c23913fb6d567854e3
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-04-18 17:16:54 +0900

    Feat: Add Stack

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
 
+// Splash 화면 (진입 화면)
 export default function Index() {
+    const router = useRouter();
+
+    // 3초 후 온보딩 화면으로 이동
+    // useEffect(() => {
+    //     const timeout = setTimeout(() => {
+    //         router.replace("/(auth)/onboarding");
+    //     }, 3000);
+    //     return () => clearTimeout(timeout);
+    // }, []);
+
     return (
-        <View className="flex-1 justify-center items-center">
+        <SafeAreaView className="flex-1 flex-col items-center bg-background-main">
             <Text className="text-4xl text-main-normal font-pretendard-bold">
-                안녕하셍
+                Splash Screen 입니다
             </Text>
-        </View>
+        </SafeAreaView>
     );
 }
